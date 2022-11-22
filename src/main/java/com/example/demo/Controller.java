ed823b9d1cff4f1e471d9b0d484d7f3ccf3b6556
Arseeniy <khusarseniy@yandex.ru>
2022-11-23 00:40:19 +0400

    fix-inject commit

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 @org.springframework.stereotype.Controller
+@Component
 public class Controller {
 
     private final MyService service = new MyService();
@@ -24,6 +26,9 @@ public class Controller {
     @GetMapping("/get/random/{min}/{max}")
     @ResponseBody
     public ResponseEntity<RandomNumber> getRandomNumber(@PathVariable Integer min, @PathVariable Integer max) {
-        return service.getRandomNumber(min, max);
+        if (min < 0) {
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+        return new ResponseEntity<>(service.getRandomNumber(), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/demo/RandomNumberApplication.java
+++ src/main/java/com/example/demo/RandomNumberApplication.java
@@ -6,10 +6,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
-public class DemoApplication {
+public class RandomNumberApplication {
 
 	public static void main(String[] args) {
-			SpringApplication.run(DemoApplication.class, args);
+			SpringApplication.run(RandomNumber.class, args);
 	}
 
 }
