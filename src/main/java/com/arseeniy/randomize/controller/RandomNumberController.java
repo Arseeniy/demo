4aa587b5158bdc2905554cc7c30272cf23373bd1
Arseeniy <123qwe.@gmail.com>
2022-11-30 03:44:41 +0400

    commit for patch test

--- src/main/java/com/arseeniy/randomize/controller/RandomNumberController.java
+++ src/main/java/com/arseeniy/randomize/controller/RandomNumberController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class RandomNumberController {
 
+
     @Autowired
     private RandomNumberService randomNumberService;
     @Autowired
