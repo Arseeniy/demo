8ed71c09edc01380a7d5e7f0b44efdd4f4946133
Arseeniy <123qwe.@gmail.com>
2022-11-30 00:57:31 +0400

    fourth_test_commit

--- src/main/java/com/arseeniy/randomize/controller/RandomNumberController.java
+++ src/main/java/com/arseeniy/randomize/controller/RandomNumberController.java
@@ -16,8 +16,8 @@ public class RandomNumberController {
     private RandomNumberService randomNumberService;
     @Autowired
     private RequestHistoryService requestHistoryService;
-    //changes for third_test_branch
-    //123123123
+    //changes for third_test_branch СПРОВОЦИРУЕМ КОНФЛИКТ
+    //123123123 СПРОВОЦИРУЕМ КОНФЛИКТ
 
     @GetMapping("/get/random/{min}/{max}/{name}")
     public ResponseEntity getRandomNumber(@PathVariable Integer min, @PathVariable Integer max,
